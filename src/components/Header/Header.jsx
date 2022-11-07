5857dabf0b68eeb167e1d3ec060e962e3fc4fc7b
Ian Hart <ianhart13@gmail.com>
2022-11-07 16:41:32 -0500

    created and styled List componenet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core'
-import SearchIcon from '@material-ui//Search';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './styles';
 
 const Header = () => {
+	const classes = useStyles();
 	return (
 		<AppBar position='static'>
 			<Toolbar className={classes.toolbar}>
@@ -14,14 +16,14 @@ const Header = () => {
 					<Typography variant="h6" className={classes.title}>
 						Explore New Places
 					</Typography>
-					<Autocomplete>
+					{/*<Autocomplete>*/}
 						<div className={classes.search}>
 							<div className={classes.searchIcon}>
 								<SearchIcon />
 							</div>
 							<InputBase placeholder='Search...' classes={{ root: classes.inputRoot, input: classes.inputInput}}/>
 						</div>
-					</Autocomplete>
+					{/*</Autocomplete>*/}
 				</Box>
 			</Toolbar>
 		</AppBar>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+import useStyles from './styles';
 
 export const Map = () => {
+	const classes = useStyles();
+	const isMobile = useMediaQuery('(min-width:600px)');
+	const coordinates = { lat: 0, lng: 0 }
+
+
 	return (
-		<div>Map</div>
+		<div className={classes.mapContainer}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: 'AIzaSyBmW3FcN3NvJU8fcrei5tPNvWh8oAQw3AQ'}}
+				defaultCenter={coordinates}
+				center={coordinates}
+				defaultZoom={14}
+				margin={[50, 50, 50, 50]}
+				options={''}
+				onChange={''}
+				onChildClick={''}
+				>
+
+
+			</GoogleMapReact>
+
+		</div>
 	)
 }
